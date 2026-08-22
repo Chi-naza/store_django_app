3db2e26eb18331d2882c6834db32b4de038892d2
Chi-naza <programmerchinaza@gmail.com>
2026-08-22 16:07:37 +0100

    Added Two Apps -  Store & Tags | Created DB Models and Made Migrations

--- store/models.py
+++ store/models.py
@@ -1,3 +1,138 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey("Product", on_delete=models.SET_NULL, null=True, related_name="+")
+
+class Product(models.Model):
+    BRONZE = "B"
+    SILVER = "S"
+    GOLD = "G"
+
+    MEMBERSHIP_CHOICES = [
+        (BRONZE, "Bronze"),
+        (SILVER, "Silver"),
+        (GOLD, "Gold"),
+    ]
+
+    # custom_id = models.CharField(max_length=20, primary_key= True) # if you want another pk aside the auto id created by django
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=BRONZE)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT) # With PROTECT, deleting a collection doesn't delete all the products in it
+    promotions = models.ManyToManyField(Promotion, related_name="products") # So one product can have multiple promotions vice versa; the related name overrides the default text Django will use to store products (product_set) in the Promotion Table. 
+
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    birth_date = models.DateField(null=True)
+
+
+
+class Order(models.Model):
+    PENDING = "P"
+    COMPLETE = "C"
+    FAILED = "F"
+
+    PAYMENT_STATUS = [
+        (PENDING, "Pending"),
+        (COMPLETE, "Complete"),
+        (FAILED, "Failed"),
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS, default=PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    # Case where a customer can have only one address
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True) # We can have SET_NULL -  to set this field NULL after customer is deleted, SET_DEFAULT - to set a default value to this field after customer is deleted, PROTECT - to protect deletion
+    # Case where a customer can have many/multiple addresses. NB -  the pk field is removed to allow many-to-one relationship
+    # customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+
+
+
+
+
+"""
+NB: Product has a circular dependency on Collection | A collection can have a featured_product but the field is nullable
+
+In Django models.py files, this usually happens when Model A has a ForeignKey to Model B, and Model B simultaneously has a ForeignKey to Model A.
+
+Imagine you have two apps: orders and customers:
+
+#### Inside orders/models.py
+from customers.models import Customer  # <-- Imports Customer
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    
+#### Inside customers/models.py
+from orders.models import Order  # <-- Crucial mistake: Imports Order back!
+
+class Customer(models.Model):
+    favorite_order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+
+
+PROBLEM: When Django starts, it reads orders/models.py, which stops to read customers/models.py, which stops to read orders/models.py... Python gets stuck in a loop and crashes.
+
+SOLUTION: Pass the relationship as a String format...this tells Python -- "Don't look for this model right now during import. Wait until all apps are fully loaded, then link them together.
+
+BELOW ARE THE SOLVED MODELS:
+
+# Inside orders/models.py
+class Order(models.Model):
+    # No import needed! Django finds it via the app name string
+    customer = models.ForeignKey('customers.Customer', on_delete=models.CASCADE)
+
+# Inside customers/models.py
+class Customer(models.Model):
+    # No import needed! 
+    favorite_order = models.ForeignKey('orders.Order', on_delete=models.SET_NULL, null=True)
+
+
+
+"""
+
+
+
--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+
+"""
+This Tag Model and TagItem is an example of Generic Relationships. 
+Instead of importing different models from other apps within our django project,
+we instead create a generic foreign key, with fields we can use to trap and find any Object/Model we apply it to.
+To Identify the parent object, we just need the type of the object, and the id of that object. With these we can lookup the DB
+
+This means, it can be applied to any model object.
+
+
+This same solution can be used to creat a Like Model with LikeItem, so with that, we can track what object a user likes
+
+NB: this solution fails if the parent object uses a customId or field as a primary key i.e when primary key is not an integer.
+"""
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TagItem(models.Model):
+    # what tag is applied to what object
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveSmallIntegerField()
+    content_object = GenericForeignKey() # this extra field is used to get the actual object this relationship is applied to
